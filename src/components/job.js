0537e48f7e77e8234e11fa936bb90333b40bb586
newtron_ahmed <bazubairu@st.ug.edu.gh>
2020-12-19 00:43:29 +0000

    fixed responsiveness

--- src/components/job.js
+++ src/components/job.js
@@ -5,9 +5,9 @@
  const JobComponent = styled.div`
 border-radius:3px;
 
-padding:1rem;
+padding:1.5rem;
 box-shadow: 0  4px 4px rgba(0, 0, 0, 0.2);
-margin:1.5rem;
+margin:2rem 1rem;
 background:#fff;
 border-left : ${({featured})=>{
 	return featured ? '5px solid hsl(180, 29%, 50%)' : '0px';
@@ -21,18 +21,20 @@ const NewBadge = styled.div`
 background-color: hsl(180, 29%, 50%);
 color:white;
 border-radius:15px;
-padding:2px 5px;
+font-size:15px;
+padding:2.5px 5px;
 font-weight:bold;
-margin:0 5px;
+margin-left: 5px;
 text-align:center;
 `
 const FeaturedBadge = styled.div`
 background-color:hsl(180, 14%, 20%);
 color:white;
+font-size:15px;
 border-radius:15px;
-padding:2px 5px;
+padding:2.5px 5px;
 font-weight:bold;
-margin:0 5px;
+margin-left:5px;
 text-align:center;
 
 `
@@ -58,14 +60,14 @@ const Job = ({company,role,position,level,postedAt,contract,languages,tools,loca
         	<div  className='avatar' >
                <img src={logo} alt='logo' style={{width:'100%'}}/>
             </div>
-        	<div style={{marginLeft:'20px'}} >
+        	<div style={{marginLeft:'10px'}} >
         		<div className ='pad' style={{display:'flex'}}>
         			<div style={{color:'hsl(180, 29%, 50%)',fontWeight:'bold'}}>{company}</div>
         			<div>{isNew ?<NewBadge>NEW!</NewBadge>:''}</div>
         			<div>{featured ? <FeaturedBadge>FEATURED</FeaturedBadge> : ''}</div>
         		</div>
         		<div style={{fontWeight:'bold',margin:'3px 0px'}}>{position}</div>
-        		<div style={{display:'flex',color:'rgba(0,0,0,0.3)',flexWrap:'wrap'}}>
+        		<div style={{display:'flex',color:'rgba(0,0,0,0.4)',flexWrap:'wrap'}}>
         			<div style={{marginRight:'10px'}}>{postedAt} <span >&bull;</span></div>
         			<div style={{marginRight:'10px'}}>{contract} <span >&bull;</span></div>
         			<div style={{marginRight:'10px'}}>{location} </div>
@@ -75,7 +77,7 @@ const Job = ({company,role,position,level,postedAt,contract,languages,tools,loca
 
         </div>
                 <div className="hr"></div>
-    		<div style={{display:'flex',flexWrap:'wrap',alignContent:'center'}}>
+    		<div style={{display:'flex',flexWrap:'wrap',alignContent:'center',marginLeft:'10px'}} >
     		{languages.map(lang=>{ return <Description onClick={()=>addFilter(lang)}>{lang} </Description>})}
     		{tools.map(tool=>{ return <Description onClick={()=>addFilter(tool)}>{tool} </Description>})}
     		{<Description onClick={()=>addFilter(role)}>{role}</Description>}
